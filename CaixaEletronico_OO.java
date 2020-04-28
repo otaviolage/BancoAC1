c4dec1f7d67604eabc5aaf71a7d927ac43cb39f1
Otávio Lage <63686965+otaviolage@users.noreply.github.com>
2020-04-28 17:29:47 -0300

    Add files via upload

---
+++ CaixaEletronico_OO.java
@@ -0,0 +1,154 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CaixaEletronico_OO {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("#,###.00");
+		Conta conta = new Conta(123456789, 123456);
+
+		conta.setBanco(100);
+		conta.setAgencia(3003);
+		conta.setConta(1015582);
+
+		boolean repeatTodaOperacao = true;
+
+		while (repeatTodaOperacao == true) {
+
+			System.out.println("==========================================");
+			System.out.println("CAIXA ELETRÔNICO DO BANCO DO OTÁVIO");
+			System.out.println("==========================================\n");
+			
+			System.out.print("Digite o seu CPF: ");
+			int cpf = ler.nextInt();
+
+			while (cpf != conta.getCpf()) { // testa o cpf
+				System.out.println("\nCPF INCORRETO");
+				System.out.print("Digite o seu CPF novamente: ");
+				cpf = ler.nextInt();
+			}
+
+			System.out.print("Senha: ");
+			int senha = ler.nextInt();
+
+			for (int i = 1; i <= 2 && senha != conta.getSenha(); i++) {
+				System.out.print("\nSENHA INCORRETA\nDigite novamente: ");
+				senha = ler.nextInt();
+				if (i == 2 && senha != conta.getSenha()) {
+					System.out.println("\nCPF BLOQUEADO");
+					System.out.println("Terceira tentativa incorreta");
+					
+					System.out.println("\nLigue no seu banco para desbloqueá-lo");
+					System.out.println("        (15) 99999-9999");
+					System.exit(1);
+				}
+			}
+
+			// tela de boas vindas
+
+			System.out.println("\n==========================================\n");
+			System.out.println("Bem-vindo Cliente!");
+
+			System.out.println("Banco: " + conta.getBanco());
+			System.out.println("Ag: " + conta.getAgencia());
+			System.out.println("Conta: " + conta.getConta());
+
+			// final de boas vindas
+
+			boolean repeatOperacoes = true;
+
+			while (repeatOperacoes == true) {
+				// menu:
+				
+				System.out.println("\n==========================================\n");
+				
+				System.out.println("Digite 1 para visualizar o saldo"); // getSaldo
+				System.out.println("Digite 2 para depósitos"); // deposita
+				System.out.println("Digite 3 para saques"); // saque
+				System.out.println("Digite 4 para transferências"); // transferir
+				System.out.println("Digite 5 para extrato"); // extrato
+				System.out.println("Digite 0 para sair");
+				System.out.print("Digite a operacao que deseja realizar: ");
+				int operacao = ler.nextInt();
+
+				System.out.println("\n==========================================\n");
+
+				// final de menu
+
+				// operacoes:
+				switch (operacao) {
+				case 1: // SALDO
+					System.out.println("Seu saldo atual é: R$" + df.format(conta.getSaldo()));
+					break;
+				case 2: // DEPÓSITO
+					System.out.print("Digite o valor que deseja depositar: ");
+					double valorDeposito = ler.nextInt();
+					conta.deposita(valorDeposito);
+					break;
+				case 3: // SAQUE
+					System.out.print("Digite o valor deseja sacar: ");
+					double valorSaque = ler.nextInt();
+					conta.saque(valorSaque);
+					break;
+				case 4: // TRANSFERÊNCIA
+					// double valor, Conta contaDestino, Conta agencia, Conta banco
+					System.out.print("Digite o valor deseja transferir: ");
+					double valorTransferencia = ler.nextDouble();
+					System.out.print("Digite o CPF do destinatário: ");
+					int cpfDestinatario = ler.nextInt();
+					Conta contaDestino = new Conta(cpfDestinatario);
+					System.out.print("Digite o número da conta: ");
+					int numeroContaDestino = ler.nextInt();
+					contaDestino.setConta(numeroContaDestino);
+					System.out.print("Agencia: ");
+					int numeroAgenciaDestino = ler.nextInt();
+					contaDestino.setAgencia(numeroAgenciaDestino);
+					System.out.print("Número do Banco: ");
+					int numeroBancoDestino = ler.nextInt();
+					contaDestino.setBanco(numeroBancoDestino);
+					conta.transferir(valorTransferencia, contaDestino.getConta(), contaDestino.getAgencia(),
+							contaDestino.getBanco());
+					break;
+				case 5: // EXTRATO
+					conta.extrato();
+					break;
+				case 0: // SAIR
+					repeatOperacoes = false;
+					break;
+				default: // INCORRETO
+					System.out.println("Comando incorreto, inicie novamente");
+				} // fim de switch operacao
+
+				// comando final:
+				System.out.println("\n==========================================");
+				System.out.println("Deseja realizar uma nova operação?");
+				System.out.println("Digite [sim] para realizar");
+				System.out.println("Digite [não] para reiniciar");
+				System.out.println("Digite a senha de desligamento para finalizar");
+				System.out.println("==========================================");
+				System.out.print("- ");
+
+				String comandoFinal = ler.next();
+				switch (comandoFinal) {
+				case "sim":
+					break;
+				case "não":
+					repeatOperacoes = false;
+					break;
+				case "1234":
+					;
+					System.out.println("Máquina desligando...");
+					System.exit(1);
+					break;
+				default:
+					System.out.println("Comando incorreto, inicie novamente");
+					break;
+				} // fim de switch comandoFinal
+			} // fim de while para repeatOperacoes
+			
+			System.out.println("\n==========================================\nAdeus....\n\n\n");
+			
+		} // fim de while para repeatTodaOperacao
+	}// fim do main
+}// fim da class
---
+++ Conta.java
@@ -0,0 +1,117 @@
+import java.text.DecimalFormat;
+
+public class Conta {
+
+	DecimalFormat df = new DecimalFormat("#,###.00");
+
+	private int cpf;
+	private int banco;
+	private int agencia;
+	private int conta;
+	private int senha;
+	private double saldo = 1000;
+	private int tamanhoExtrato = 1;
+	private String[] extrato = new String[10];
+	
+	public Conta(int cpf, int senha) { // conta do usuário do caixa
+		this.cpf = cpf;
+		this.senha = senha;
+	}
+	public Conta(int cpf) { // contas para transferências
+		this.cpf = cpf;
+	}
+	
+	//getters & setters
+	public int getBanco() {
+		return banco;
+	}
+	public void setBanco(int banco) {
+		this.banco = banco;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getConta() {
+		return conta;
+	}
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getSenha() {
+		return senha;
+	}
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	} // fim de getters & setters
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+		tamanhoExtrato++;
+		extrato[tamanhoExtrato] = ((tamanhoExtrato - 1) +".depósito: " + valor);
+	}
+	
+	public boolean saque(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			tamanhoExtrato++;
+			extrato[tamanhoExtrato] = (tamanhoExtrato - 1) +".saque: -" + valor;
+			return true;
+		} else {
+			System.out.println("\n==========================================");
+			System.out.println("SALDO INSUFICIENTE");
+			System.out.println("operação não realizada");
+			System.out.println("==========================================\n");
+			return false;
+		}
+	}
+	
+	public boolean transferir(double valor, int conta, int agencia, int banco) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			tamanhoExtrato++;
+			extrato[tamanhoExtrato] = (tamanhoExtrato - 1) + ".transferência: -" + valor;
+			
+			System.out.println("\n==========================================\n");
+			System.out.println("Comprovante: ");
+			System.out.println("Banco: " + banco);
+			System.out.println("Ag: " + agencia);
+			System.out.println("Conta: " + conta);
+			System.out.println("Valor Transferido: R$" + df.format(valor));
+			System.out.println("\n==========================================\n");
+			
+			return true;
+		} else {
+			System.out.println("\n==========================================");
+			System.out.println("SALDO INSUFICIENTE");
+			System.out.println("operação não realizada");
+			System.out.println("==========================================");
+			return false;
+		}
+	}
+	
+	public void extrato() {
+		System.out.println("\n==========================================\n");
+		System.out.println("Extrato da conta " + this.conta + ":\n");
+		extrato[1] = "Saldo inicial: R$ 1.000,00";
+		for (int i = 1; i <= tamanhoExtrato; i++) {
+			System.out.println(extrato[i]);
+		}
+		System.out.println("Saldo atual: R$" + df.format(saldo));
+	}
+
+}
